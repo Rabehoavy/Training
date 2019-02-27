8e840af35d06a312c8f842e16ee8d2a0b1043d88
Rabehoavy <rabehoavy@users.noreply.github.com>
2019-02-27 11:54:27 +0100

    Add bootstrap

--- php/php-mysql-crud-master/read.php
+++ php/php-mysql-crud-master/read.php
@@ -3,12 +3,77 @@
   <head>
     <meta charset="utf-8">
     <title>Randonnées</title>
-    <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </head>
   <body>
-    <h1>Liste des randonnées</h1>
-    <table>
-      <!-- Afficher la liste des randonnées -->
-    </table>
+  <nav class="navbar navbar-expand-lg navbar-primary bg-light">
+    <a class="navbar-brand" href="#">Randonnée</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="read.php">Liste des données <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="create.php">Ajouter des données</a>
+        </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+  </nav>
+    <?php
+        echo "<div class='row'><div class='offset-md-3 col-md-6'><h1>Liste des randonnées</h1><table  class='table table-primary text-success'>";
+        echo "<tr><th scope='col'>Nom</th><th scope='col'>Difficulté</th><th scope='col'>Distance (km)</th><th scope='col'>Durée</th><th scope='col'>Dénivelé (m)</th></tr>";
+
+        class TableRows extends RecursiveIteratorIterator { 
+            function __construct($it) { 
+                parent::__construct($it, self::LEAVES_ONLY); 
+            }
+
+            function current() {
+                return "<td>" . parent::current(). "</td>";
+            }
+
+            function beginChildren() { 
+                echo "<tr>"; 
+            } 
+
+            function endChildren() { 
+                echo "</tr>" . "\n";
+            } 
+        } 
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "reunion_island";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT name, difficulty, distance, duration, height_difference FROM hiking"); 
+            $stmt->execute();
+
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+            foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+                echo $v;
+            }
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        echo "</table></div></div>";
+    ?>
   </body>
 </html>
